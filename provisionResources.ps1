6b5617391693c5b0cbeca39713d3dc7777254c4d
Mart <amnwhitejr@gmail.com>
2020-08-11 14:37:56 -0400

    added echos after steps

--- provisionResources.ps1
+++ provisionResources.ps1
@@ -27,19 +27,24 @@ az vm open-port --port 443
 
 # provision KV
 az keyvault create -n "$kvName" --enable-soft-delete false --enabled-for-deployment true
+echo "KV created"
 
 # TODO: create KV secret (database connection string)
 az keyvault secret set --vault-name "$kvName" -n "$kvSecretName" --value "$kvSecretValue"
+echo "secret created"
 
 # TODO: set KV access-policy (using the vm ``systemAssignedIdentity``)
 az keyvault set-policy -n "$kvName" --object-id "$systemAssignedIdentity" --secret-permissions get list
+echo "policy created, starting 1configure"
 
 az vm run-command invoke --command-id RunShellScript --scripts @vm-configuration-scripts/1configure-vm.sh
+echo "1configure complete, starting 2configure"
 
 az vm run-command invoke --command-id RunShellScript --scripts @vm-configuration-scripts/2configure-ssl.sh
+echo "2configure complete, starting deploy"
 
 az vm run-command invoke --command-id RunShellScript --scripts @deliver-deploy.sh
-
+echo "deploy complete"
 
 # TODO: print VM public IP address to STDOUT or save it as a file
 #ifconfig
